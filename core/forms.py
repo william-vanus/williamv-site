2d9bc01c4d29694c698c79aca050cacb6fce02b1
William Vanus <william.vanus.pro@gmail.com>
2022-02-03 17:11:08 -0300

    Add contact form

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,7 @@
-from django.forms import Form, ModelForm
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    message = forms.CharField(label='Mensagem', widget=forms.Textarea())
--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .models import Portfolio
+from .forms import ContactForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['portfolios'] = Portfolio.objects.order_by('?').all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        #
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        #
+        messages.error(self.request, 'Falha ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
