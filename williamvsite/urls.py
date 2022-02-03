9a7e670dacb7350b8998648055bc1633fde9d81c
William Vanus <william.vanus.pro@gmail.com>
2022-02-03 14:37:51 -0300

    Securing admin url

--- williamvsite/urls.py
+++ williamvsite/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from decouple import config
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(config('ADMIN_URL'), admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
